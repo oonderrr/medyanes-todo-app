fe39e528eb46692fdc474a4996c5488f570a569e
Önder BUĞDACİ <onder.bugdaci@gmail.com>
2025-09-06 11:52:57 +0300

    react query eklendi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import todosService from '@/services/todos/todos.service';
+'use client';
 
-const page = async () => {
-  const data = await todosService.getAllTodos();
+import todosService from '@/services/todos/todos.service';
+import { useQuery } from '@tanstack/react-query';
+
+const Page = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['get-all-todos'],
+    queryFn: () => todosService.getAllTodos(),
+  });
+
+  if (isLoading) {
+    return <div>Yükleniyor...</div>;
+  }
+
+  if (error) {
+    return <div>Hata: {error.message}</div>;
+  }
+
+  if (!data) {
+    return <div>Veri yok</div>;
+  }
 
   return <div>{JSON.stringify(data.todos.map((todo) => todo.title))}</div>;
 };
 
-export default page;
+export default Page;
